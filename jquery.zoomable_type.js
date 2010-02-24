97c4a519f7a53b27d8a6157c3c258488355fb9af
Tate Johnson <tate@tatey.com>
2010-02-24 23:48:07 +1000

    Preparing for first release.

---
+++ jquery.zoomable_type.js
@@ -0,0 +1,66 @@
+/*
+* Zoomable Type: Mac OS X's "Show in Large Type" for jQuery
+* @version 0.1
+* @require jQuery >= v1.4.2 
+*
+* Zoomable Type is a jQuery plugin for reading small text on a
+* display from far away.
+*
+* Distributed under the MIT licence:
+* http://www.opensource.org/licenses/mit-license.php
+*
+* Copyright (c) 2010 Tate Johnson <tate@tatey.com>
+*/
+
+(function($) {  
+  $.zoomableType = function(selector) {
+    $(selector || '.zoomable').each(function() { $(this).click(onZoom); });
+  };
+  
+  function onZoom() {
+    if ($('#zoomed').length > 0) {
+      onHide();
+      return false; 
+    }
+    var element = $('<span>', {id: 'zoomed'});
+    element.text($(this).text());
+    element.appendTo('body');
+    element.data.unzoomedWidth  = element.width();
+    element.data.unzoomedHeight = element.height();
+    setSizeAndPositionFor(element);
+    $(document).click(onHide);
+    $(window).resize(onWindowResize);
+    return false;
+  }
+  
+  function onHide() {
+    $(document).unbind('click', onHide);
+    $(window).unbind('resize', onWindowResize);
+    $('#zoomed').remove();
+  }
+  
+  function onWindowResize() {
+    setSizeAndPositionFor($('#zoomed'));
+  }
+  
+  function setSizeAndPositionFor(element) {
+    var win         = $(window);
+    var widthScale  = (win.width() - win.width() * 0.10) / element.data.unzoomedWidth;
+    var heightScale = (win.height() - win.height() * 0.10) / element.data.unzoomedHeight;
+    element.css(
+      'font-size',
+      (widthScale < heightScale ? widthScale : heightScale) * 100 + '%'
+    );
+    var radius = element.outerWidth() * 0.02;
+    element.css({
+      'padding': radius,
+      '-moz-border-radius': radius,
+      '-webkit-border-radius': radius,
+      'text-shadow': '#000 2px 2px ' + parseFloat(element.css('fontSize')) * 0.01 + 'px'
+    });
+    element.css({
+      'left': (win.width() - element.outerWidth()) / 2,
+      'top': win.height() / 2 - element.outerHeight() / 2
+    });
+  }
+})(jQuery);

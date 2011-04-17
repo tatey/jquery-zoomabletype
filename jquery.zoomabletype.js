bd65c79f7cac5ac46e36f486c6cf59c81d0f6979
Tate Johnson <tate@tatey.com>
2011-04-17 17:16:11 +1000

    Stylize and cache document + window

--- jquery.zoomabletype.js
+++ jquery.zoomabletype.js
@@ -1,46 +1,77 @@
 (function($) {
-  var ZoomableType = function(element) { 
-    this.copy     = null;
-    this.original = element.bind('click.z', this, function(e) { 
-      if (e.data.copy) return;
-      e.data.zoom();
-      e.stopPropagation();
-    });
+  var ZoomableType = {
+    d: $(document),
+    w: $(window),
+    
+    Element: function(element) {
+      this.copy     = null;
+      this.original = element.bind('click.zt', this, function(e) {
+        if (this.copy != null) return;
+        e.data.zoom();
+        e.stopPropagation();
+      });
+    }
   }
 
-  $.extend(ZoomableType.prototype, {
+  $.extend(ZoomableType.Element.prototype, {
     zoom: function() {
       this.copy = $('<span>', {className: 'zoomed'})
         .text(this.original.text())
         .appendTo('body')
-        .bind('click.z', function(e) { e.stopPropagation(); });
+        .bind('click.zt', function(e) { e.stopPropagation(); });
         
       this.resize();
-        
-      $(document).bind('click.z keyup.z', this, function(e) { e.data.shrink(); });
-      $(window).bind('resize.z', this, function(e) { e.data.resize(); });
+              
+      ZoomableType.d.bind('click.zt keyup.zt', this, function(e) { 
+        if (!e.keyCode || e.keyCode == 27) e.data.shrink();
+      });
+      ZoomableType.w.bind('resize.zt', this, function(e) { 
+        e.data.resize(); 
+      });
     },
     
     shrink: function() {      
       this.copy.remove();
       this.copy = null;
       
-      $(document).unbind('.z');
-      $(window).unbind('.z');
+      ZoomableType.d.unbind('.zt');
+      ZoomableType.w.unbind('.zt');
     },
     
     resize: function() {
-      var w = $(window),
-          wScale = w.width() * 0.9 / this.original.width(),
-          hScale = w.height() * 0.9 / this.original.height();
+      var width  = ZoomableType.w.width() * 0.9 / this.original.width() * 100,
+          height = ZoomableType.w.height() * 0.9 / this.original.height() * 100;
+      
+      this.copy.css('font-size', (width < height ? width : height) + '%');
+      this.stylize().center();
+    },
+        
+    stylize: function() {
+      var radius = this.copy.outerWidth() * 0.02;
       
       this.copy.css({
-        fontSize: (wScale < hScale ? wScale : hScale) * 100 + '%'
+        padding: radius,
+        webkitBorderRadius: radius,
+        mozBorderRadius: radius,
+        borderRadius: radius,
+        textShadow: '#000 2px 2px ' + radius + 'px'
       });
+      
+      return this;
+    },
+    
+    center: function() {
+      this.copy.css({
+        position: 'fixed',
+        left: (ZoomableType.w.width() - this.copy.outerWidth()) / 2,
+        top: (ZoomableType.w.height() - this.copy.outerHeight()) / 2
+      });
+      
+      return this;
     }
   });
-  
+    
   $.fn.zoomabletype = function() {
-    return this.each(function() { new ZoomableType($(this)); });
+    return this.each(function() { new ZoomableType.Element($(this)); });
   }
 })(jQuery);
